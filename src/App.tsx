39a8696f189e71d9ecbf62c319a88a9686332b9f
truongnv <truongnv@ponos-tech.com>
2020-08-13 21:41:47 +0700

    Implement Board#initGame()

--- src/App.tsx
+++ src/App.tsx
@@ -96,22 +96,82 @@ class King extends Piece {
   }
 }
 
+class Queen extends Piece {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(isWhite: boolean) {
+    super(isWhite);
+  }
+
+  canMove(board: Board, spotStart: Spot, spotEnd: Spot): boolean {
+    //To do
+    return false;
+  } 
+}
+
+class Knight extends Piece {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(isWhite: boolean) {
+    super(isWhite);
+  }
+
+  canMove(board: Board, spotStart: Spot, spotEnd: Spot): boolean {
+    //To do
+    return false;
+  } 
+}
+
+class Bishop extends Piece {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(isWhite: boolean) {
+    super(isWhite);
+  }
+
+  canMove(board: Board, spotStart: Spot, spotEnd: Spot): boolean {
+    //To do
+    return false;
+  } 
+}
+
+class Rook extends Piece {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(isWhite: boolean) {
+    super(isWhite);
+  }
+
+  canMove(board: Board, spotStart: Spot, spotEnd: Spot): boolean {
+    //To do
+    return false;
+  } 
+}
+
+class Pawn extends Piece {
+  // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+  constructor(isWhite: boolean) {
+    super(isWhite);
+  }
+
+  canMove(board: Board, spotStart: Spot, spotEnd: Spot): boolean {
+    //To do
+    return false;
+  } 
+}
+
 class Spot {
-  private _piece!: Piece;
+  private _piece!: Piece | null;
   private _x!: number;
   private _y!: number;
 
-  constructor(piece: Piece, x: number, y: number) {
+  constructor(piece: Piece | null, x: number, y: number) {
     this.piece = piece;
     this.x = x;
     this.y = y;
   }
 
-  get piece() : Piece {
+  get piece() : Piece | null {
     return this._piece;
   }
 
-  set piece(newValue: Piece) {
+  set piece(newValue: Piece | null) {
     this._piece = newValue;
   }
 
@@ -140,5 +200,49 @@ class Board {
       throw new Error('Out of index');
     }
     return this.boxes[x][y];
+  }
+
+  initGame() {
+    this.boxes[0][0] = new Spot(new Rook(true), 0, 0);
+    this.boxes[0][1] = new Spot(new Knight(true), 0, 1);
+    this.boxes[0][2] = new Spot(new Bishop(true), 0, 2);
+    this.boxes[0][3] = new Spot(new Queen(true), 0, 3);
+    this.boxes[0][4] = new Spot(new King(true), 0 , 4);
+    this.boxes[0][5] = new Spot(new Bishop(true), 0, 5);
+    this.boxes[0][6] = new Spot(new Knight(true), 0, 6);
+    this.boxes[0][7] = new Spot(new Rook(true), 0, 7);
+
+    this.boxes[1][0] = new Spot(new Pawn(true), 1, 0);
+    this.boxes[1][1] = new Spot(new Pawn(true), 1, 1);
+    this.boxes[1][2] = new Spot(new Pawn(true), 1, 2);
+    this.boxes[1][3] = new Spot(new Pawn(true), 1, 3);
+    this.boxes[1][4] = new Spot(new Pawn(true), 1, 4);
+    this.boxes[1][5] = new Spot(new Pawn(true), 1, 5);
+    this.boxes[1][6] = new Spot(new Pawn(true), 1, 6);
+    this.boxes[1][7] = new Spot(new Pawn(true), 1, 7);
+
+    for (var i = 2; i < 6; i++) { 
+      for (var j = 0; j < 8; j++) { 
+          this.boxes[i][j] = new Spot(null, i, j); 
+      } 
+    } 
+
+    this.boxes[6][0] = new Spot(new Pawn(false), 1, 0);
+    this.boxes[6][1] = new Spot(new Pawn(false), 1, 1);
+    this.boxes[6][2] = new Spot(new Pawn(false), 1, 2);
+    this.boxes[6][3] = new Spot(new Pawn(false), 1, 3);
+    this.boxes[6][4] = new Spot(new Pawn(false), 1, 4);
+    this.boxes[6][5] = new Spot(new Pawn(false), 1, 5);
+    this.boxes[6][6] = new Spot(new Pawn(false), 1, 6);
+    this.boxes[6][7] = new Spot(new Pawn(false), 1, 7);
+
+    this.boxes[7][0] = new Spot(new Rook(false), 0, 0);
+    this.boxes[7][1] = new Spot(new Knight(false), 0, 1);
+    this.boxes[7][2] = new Spot(new Bishop(false), 0, 2);
+    this.boxes[7][3] = new Spot(new Queen(false), 0, 3);
+    this.boxes[7][4] = new Spot(new King(false), 0 , 4);
+    this.boxes[7][5] = new Spot(new Bishop(false), 0, 5);
+    this.boxes[7][6] = new Spot(new Knight(false), 0, 6);
+    this.boxes[7][7] = new Spot(new Rook(false), 0, 7);
   }
 }
